4123d27053ec40fe19ccd3e7644066e55d148d9c
rgba16f <82187279+rgba16f@users.noreply.github.com>
2022-04-25 11:17:48 -0500

    Initial revision of python scripting for jenkins perf reporting
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>

---
+++ Project/Code/PythonTests/Automated/benchmark_runner_periodic_suite_vulkan.py
@@ -0,0 +1,80 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+"""
+import logging
+import os
+import subprocess
+import psutil
+
+import pytest
+
+import ly_test_tools.environment.process_utils as process_utils
+import ly_test_tools.launchers.platforms.base
+from ly_test_tools.benchmark.data_aggregator import BenchmarkDataAggregator
+
+logger = logging.getLogger(__name__)
+
+def filebeat_service_running():
+    """
+    Checks if the filebeat service is currently running on the OS.
+    :return: True if filebeat service detected and running, False otherwise.
+    """
+    result = False
+    try:
+        filebeat_service = psutil.win_service_get('filebeat')
+        filebeat_service_info = filebeat_service.as_dict()
+        if filebeat_service_info['status'] == 'running':
+            result = True
+    except psutil.NoSuchProcess:
+        return result
+
+    return result
+
+
+class LoftSampleException(Exception):
+    """Custom Exception class for LoftSample tests."""
+    pass
+
+
+@pytest.mark.parametrize('launcher_platform', ['windows'])
+@pytest.mark.parametrize("project", ["LoftSample"])
+@pytest.mark.usefixtures("clean_loftsample_gamelauncher_logs", "loftsample_gamelauncher_log_monitor")
+class TestPerformanceBenchmarksPeriodicSuite:
+
+    @pytest.mark.parametrize('rhi', ['-rhi=vulkan'])
+    def test_LoftSampleFrameTimingTest_GatherBenchmarkMetrics_Vulkan(
+            self, request, workspace, launcher_platform, rhi, loftsample_gamelauncher_log_monitor):
+        cmd = os.path.join(workspace.paths.build_directory(),
+                           'LoftSample.GameLauncher.exe '
+                           f'--project-path={workspace.paths.project()} '
+                           '--rhi vulkan ',
+                           '--regset="/O3DE/Performance/FrameTimeRecording/Activate=true"',
+                           '--regset="/O3DE/Performance/FrameTimeRecording/QuitOnComplete=true"',
+                           '--regset="/O3DE/Performance/FrameTimeRecording/ProfileName=LoftSampleFrameTime_vulkan"',
+                           '+loadlevel levels\archvis\loft\interior_03.spawnable')
+
+        def teardown():
+            process_utils.kill_processes_named(['AssetProcessor', 'LoftSample.GameLauncher'], ignore_extensions=True)
+        request.addfinalizer(teardown)
+
+        # Execute test.
+        process_utils.safe_check_call(cmd, stderr=subprocess.STDOUT, encoding='UTF-8', shell=True)
+        try:
+            expected_lines = ["Script: OutputProfileData complete"]
+            loftsample_gamelauncher_log_monitor.monitor_log_for_lines(expected_lines, timeout=180)
+        except ly_test_tools.log.log_monitor.LogMonitorException as e:
+            raise LoftSampleException(f'Data capturing did not complete in time for RHI {rhi}, got error: {e}')
+
+    @pytest.mark.skipif(not filebeat_service_running(), reason="filebeat service not running")
+    def test_LoftSampleFrameTimingTest_SendBenchmarkMetrics_Vulkan(
+            self, request, editor, workspace, project, launcher_platform, level):
+        """
+        Gathers the Vulkan benchmark metrics and uses filebeat to send the metrics data.
+        """
+
+        aggregator = BenchmarkDataAggregator(workspace, logger, 'periodic')
+        aggregator.upload_metrics('vulkan')
+
---
+++ Project/Code/PythonTests/conftest.py
@@ -0,0 +1,49 @@
+"""
+Copyright (c) Contributors to the Open 3D Engine Project.
+For complete copyright and license terms please see the LICENSE at the root of this distribution.
+
+SPDX-License-Identifier: Apache-2.0 OR MIT
+
+pytest test configuration file for launching AtomSampleViewerStandalone tests.
+"""
+
+import logging
+import os
+import types
+
+import pytest
+
+import ly_test_tools.builtin.helpers as helpers
+import ly_test_tools.environment.file_system as file_system
+import ly_test_tools.log.log_monitor
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clean_loftsample_gamelauncher_logs(request, workspace):
+    """Deletes any AtomSampleViewer log files so that the test run can start with empty logs."""
+    logs = ['Game.log']
+    logger.info(f'Deleting log files for LoftSample.GameLauncher tests: {logs}')
+
+    for log in logs:
+        log_file = os.path.join(workspace.paths.project_log(), log)
+        if os.path.exists(log_file):
+            file_system.delete(file_list=[log_file],
+                               del_files=True,
+                               del_dirs=False)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def loftsample_gamelauncher_log_monitor(request, workspace):
+    """Creates a LyTestTools log monitor object for monitoring LoftSample.GameLauncher logs."""
+    def is_alive(launcher_name):
+        return True
+
+    launcher = ly_test_tools.launchers.platforms.base.Launcher(workspace, [])  # Needed for log monitor to work.
+    launcher.is_alive = types.MethodType(is_alive, launcher)
+    file_to_monitor = os.path.join(workspace.paths.project_log(), 'Game.log')
+    log_monitor = ly_test_tools.log.log_monitor.LogMonitor(launcher=launcher, log_file_path=file_to_monitor)
+    log_monitor.file_to_monitor = file_to_monitor
+
+    return log_monitor
