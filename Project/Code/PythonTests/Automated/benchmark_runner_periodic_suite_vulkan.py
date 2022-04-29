9b21bf931d155f5e01e8560229f6d53230f36c6c
rgba16f <82187279+rgba16f@users.noreply.github.com>
2022-04-29 13:11:29 -0500

    Update level to deal with parrallel changes
    Signed-off-by: rgba16f <82187279+rgba16f@users.noreply.github.com>

--- Project/Code/PythonTests/Automated/benchmark_runner_periodic_suite_vulkan.py
+++ Project/Code/PythonTests/Automated/benchmark_runner_periodic_suite_vulkan.py
@@ -12,10 +12,10 @@ from Automated.benchmark_runner_periodic_suite_common import filebeat_service_ru
 
 @pytest.mark.parametrize('launcher_platform', ['windows'])
 @pytest.mark.parametrize("project", ["LoftSample"])
+@pytest.mark.parametrize('rhi', ['vulkan'])
 @pytest.mark.usefixtures("clean_loftsample_gamelauncher_logs", "loftsample_gamelauncher_log_monitor")
 class TestPerformanceBenchmarksPeriodicSuite:
 
-    @pytest.mark.parametrize('rhi', ['-rhi=vulkan'])
     def test_LoftSampleFrameTimingTest_GatherBenchmarkMetrics_Vulkan(
             self, request, workspace, launcher_platform, rhi, loftsample_gamelauncher_log_monitor):
             LoftSampleFrameTimingTest_GatherBenchmarkMetrics_Common(self, request, workspace, rhi, loftsample_gamelauncher_log_monitor)
@@ -23,7 +23,7 @@ class TestPerformanceBenchmarksPeriodicSuite:
 
     @pytest.mark.skipif(not filebeat_service_running(), reason="filebeat service not running")
     def test_LoftSampleFrameTimingTest_SendBenchmarkMetrics_Vulkan(
-            self, request, editor, workspace, project, launcher_platform, level):
+            self, request, editor, workspace, project, rhi, launcher_platform, level):
         """
         Gathers the Vulkan benchmark metrics and uses filebeat to send the metrics data.
         """
